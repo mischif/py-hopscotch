00304e0b2d17ebccdc0ee5de278a0ddd9a20b13d
Jeremy Brown <mischif@users.noreply.github.com>
2017-04-25 22:51:43 -0400

    [skip ci] Added header and encoding pragma

--- hopscotchdict.py
+++ hopscotchdict.py
@@ -1,3 +1,12 @@
+# encoding: utf-8
+
+################################################################################
+#                              py-hopscotch-dict                               #
+#    Full-featured `dict` replacement with guaranteed constant-time lookups    #
+#                               (C) 2017 Mischif                               #
+#       Released under version 3.0 of the Non-Profit Open Source License       #
+################################################################################
+
 from __future__ import division
 from array import array
 from copy import copy
