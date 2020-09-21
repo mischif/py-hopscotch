189b6cf6857e8adf37831eb549128b14eb8f8524
Jeremy Brown <mischif@users.noreply.github.com>
2020-09-20 22:14:25 -0400

    Moved away from using setup.py

--- Makefile
+++ Makefile
@@ -7,23 +7,21 @@
 
 .POSIX:
 
-CI_OPTIONS="--cov-report xml --hypothesis-profile ci"
-
 .PHONY: ci-test clean release test typecheck
 
 clean:
-	rm -rf .coverage coverage.xml .eggs/ .hypothesis/ .pytest_cache/ *egg-info/ dist/ build/
+	rm -rf .coverage coverage.xml .eggs/ .hypothesis/ .mypy_cache/ .pytest_cache/ *egg-info/ dist/ build/
 	find . -name __pycache__ -exec rm -rf {} +
 	find . -name *.pyc -exec rm -rf {} +
 
 test:
-	python -B setup.py test
+	pytest
 
 ci-test:
-	python setup.py test --addopts ${CI_OPTIONS}
+	pytest --cov-report xml --hypothesis-profile ci
 
 release:
 	python -m pep517.build -sb .
 
 typecheck:
-	mypy --python-version 3.6 --pretty --strict --strict-optional --warn-no-return --warn-unreachable --disallow-redefinition src/
+	mypy --config-file pyproject.toml src/
--- setup.py
+++ setup.py
@@ -7,75 +7,14 @@
 #       Released under version 3.0 of the Non-Profit Open Source License       #
 ################################################################################
 
-from io import open
-from os.path import abspath, dirname, join
-from setuptools import find_packages, setup
+# Have to enable user site manually since PEP517 workflow doesn't support editable installs
+# See pypa/pip#7953 
+import site
+import sys
 
+from setuptools import setup
 
-package_root = abspath(dirname(__file__))
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
-# Get the long description from the README file
-with open(join(package_root, "README.md"), encoding="utf-8") as desc:
-	long_description = desc.read()
-
-
-setup(
-	name="py-hopscotch-dict",
-
-	packages=find_packages(where="src"),
-
-	package_dir={"": "src"},
-
-	license="NPOSL-3.0",
-
-	url="https://github.com/mischif/py-hopscotch-dict",
-
-	description="A replacement for dict using hopscotch hashing",
-
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-
-	author="Jeremy Brown",
-	author_email="mischif@users.noreply.github.com",
-
-	python_requires="~=3.6",
-
-	package_data={"py_hopscotch_dict": ["VERSION"]},
-
-	setup_requires=["pytest-runner", "setuptools_scm"],
-
-	tests_require=["hypothesis", "hypothesis-pytest", "pytest", "pytest-cov"],
-
-	zip_safe=False,
-
-	keywords=["hopscotch", "dict", "hashtable"],
-
-	extras_require={
-		"test": ["mypy"],
-		},
-
-	options={
-		"aliases": {
-			"test": "pytest",
-			},
-
-		"metadata": {
-			"license_files": "LICENSE",
-			},
-		},
-
-	classifiers=[
-		"Development Status :: 5 - Production/Stable",
-
-		"Intended Audience :: Developers",
-		"Topic :: Software Development :: Libraries :: Python Modules",
-		"Operating System :: OS Independent",
-
-		"License :: OSI Approved :: Open Software License 3.0 (OSL-3.0)",
-
-		"Programming Language :: Python :: 3",
-		"Programming Language :: Python :: 3.6",
-		"Programming Language :: Python :: 3.7",
-		"Programming Language :: Python :: 3.8",
-		],
-	)
+if __name__ == "__main__":
+	setup()
